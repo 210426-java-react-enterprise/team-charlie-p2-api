5535db6c39c41afcc52cf09ad00f8a1385a2be61
Richard <rtcloud2@gmail.com>
2021-06-03 16:19:33 -0400

    added YamlConfig

---
+++ src/main/java/com/revature/pantry/config/YamlConfig.java
@@ -0,0 +1,30 @@
+package com.revature.pantry.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties("api")
+public class YamlConfig {
+
+    private String id;
+    private String key;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
