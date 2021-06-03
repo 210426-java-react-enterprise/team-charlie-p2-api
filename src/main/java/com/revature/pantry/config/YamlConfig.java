7cc73ffc6d0428532a053de55d4edbbfb0b26d2f
Kevin <kevin.chang@revature.net>
2021-06-03 15:27:35 -0600

    added LoggingAspect and SwaggerConfig with ApiDocController

--- src/main/java/com/revature/pantry/config/YamlConfig.java
+++ src/main/java/com/revature/pantry/config/YamlConfig.java
@@ -9,22 +9,23 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties("api")
 public class YamlConfig {
 
-    private String id;
-    private String key;
+    private String app_id;
+    private String app_key;
 
-    public String getId() {
-        return id;
+    public String getApp_id() {
+        return app_id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setApp_id(String app_id) {
+        this.app_id = app_id;
     }
 
-    public String getKey() {
-        return key;
+    public String getApp_key() {
+        return app_key;
     }
 
-    public void setKey(String key) {
-        this.key = key;
+    public void setApp_key(String app_key) {
+        this.app_key = app_key;
     }
+
 }
