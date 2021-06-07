b38add4dcb36f3d17f72c5111cdd233ef6e4790b
Richard <rtcloud2@gmail.com>
2021-06-07 12:35:48 -0400

    made FavoriteRecipeReposotory use an Integer instead of a FavoriteRecipeId

--- src/main/java/com/revature/pantry/repos/FavoriteRecipeRepository.java
+++ src/main/java/com/revature/pantry/repos/FavoriteRecipeRepository.java
@@ -1,17 +1,16 @@
 package com.revature.pantry.repos;
 
 import com.revature.pantry.models.UserFavoriteRecipe;
-import com.revature.pantry.models.UserRecipeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
 import java.util.Set;
 
 @Repository
-public interface FavoriteRecipeRepository extends JpaRepository <UserFavoriteRecipe, UserRecipeId> {
+public interface FavoriteRecipeRepository extends JpaRepository <UserFavoriteRecipe, Integer> {
 
     @Modifying
     @Query(value = "from UserFavoriteRecipe where user_id = :id")
