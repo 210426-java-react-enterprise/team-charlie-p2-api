ad0d8036a1529ddafa7241099a9d6f3e46e02e55
Austin <austinknauer@alumni.colostate.edu>
2021-06-10 17:31:09 -0600

    fleshed out exception handling aspect

--- src/main/java/com/revature/pantry/aspects/ExceptionHandlerAspect.java
+++ src/main/java/com/revature/pantry/aspects/ExceptionHandlerAspect.java
@@ -3,6 +3,7 @@ package com.revature.pantry.aspects;
 
 import com.revature.pantry.exceptions.*;
 import com.revature.pantry.web.dtos.*;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.*;
@@ -27,13 +28,92 @@ public class ExceptionHandlerAspect {
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(AuthenticationException.class)
-    public void handleAuthenticationException() { }
+    public ErrorMessage handleAuthenticationException(AuthenticationException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.UNAUTHORIZED.value(),
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false)
+        );
+
+        return message;
+    }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AuthorizationException.class)
-    public void handleAuthorizationException() { }
+    public ErrorMessage handleAuthorizationException(AuthorizationException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.FORBIDDEN.value(),
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false)
+        );
+
+        return message;
+    }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ResourcePersistenceException.class)
-    public void handleResourcePersistenceException() { }
+    public ErrorMessage handleResourcePersistenceException(ResourcePersistenceException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false)
+        );
+
+        return message;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(InvalidRecipeException.class)
+    public ErrorMessage handleInvalidRecipeException(InvalidRecipeException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false)
+        );
+
+        return message;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(InvalidRequestException.class)
+    public ErrorMessage handleInvalidRequestException(InvalidRequestException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false)
+        );
+
+        return message;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MealDataIsInvalidException.class)
+    public ErrorMessage handleMealDataIsInvalidException(MealDataIsInvalidException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false)
+        );
+
+        return message;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserDataIsInvalidException.class)
+    public ErrorMessage handleUserDataIsInvalidException(UserDataIsInvalidException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false)
+        );
+
+        return message;
+    }
 }
