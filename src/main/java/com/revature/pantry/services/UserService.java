a3aa6744c7cf9bda4fbb6382c2e5d30c872e09ac
Richard <rtcloud2@gmail.com>
2021-06-11 15:21:03 -0400

    commit before pull

--- src/main/java/com/revature/pantry/services/UserService.java
+++ src/main/java/com/revature/pantry/services/UserService.java
@@ -13,6 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
+/**
+ * UserService
+ *
+ * Service class that acts as a middleman between the repository and the endpoints.
+ * Contains various functions to handle transactions and database calls.
+ *
+ * @author Richard Taylor
+ * @author Kevin Chang
+ * @author Oswaldo Castillo
+ * @author Austin Knauer
+ * @author Uros Vorkapic
+ */
 @Service
 @Transactional
 public class UserService {
@@ -26,7 +38,17 @@ public class UserService {
         this.recipeRepository = recipeRepository;
     }
 
-    public User authenticate(String username, String password) {
+    /**
+     * Takes the username and password provided and queries the database to see if valid credentials were provided.
+     *
+     * @param username The username passed in
+     * @param password The password passed in
+     * @return The authenticated user, throws an exception otherwise
+     *
+     * @author Kevin Chang
+     * @throws AuthenticationException when either credential is incorrect.
+     */
+    public User authenticate(String username, String password) throws AuthenticationException {
         User user = userRepository.findUserByUsernameAndPassword(username, password);
         if (user == null) {
             throw new AuthenticationException("You inputted an invalid username or password!");
@@ -34,12 +56,29 @@ public class UserService {
         return userRepository.findUserByUsernameAndPassword(username, password);
     }
 
-    public User findUserById(int userId) {
+    /**
+     * Finds the user in the database from the id provided. Used as a utility method for other methods.
+     *
+     * @param userId the user Id to be searched
+     * @return The user if there is one, an exception otherwise.
+     * @author Oswaldo Castillo
+     * @throws InvalidRequestException when there is no user to be found
+     */
+    public User findUserById(int userId) throws InvalidRequestException{
         return userRepository.findById(userId)
                 .orElseThrow(InvalidRequestException::new);
     }
 
-    public User registerUser(Registration newUser) {
+    /**
+     * Takes in a (bean validated) registration and uses that to create a user account.
+     *
+     * @param newUser The registration object containing user details
+     * @return The newly registered user
+     * @throws ResourcePersistenceException When either the username or email already exists within the database.
+     * @author Richard Taylor
+     * @author Kevin Chang
+     */
+    public User registerUser(Registration newUser) throws ResourcePersistenceException{
         User user = new User(newUser.getUsername(), newUser.getPassword(), newUser.getEmail());
         user.setRole(User.Role.BASIC_USER);
         User registeredUser = null;
@@ -51,7 +90,16 @@ public class UserService {
         return registeredUser;
     }
 
-    public boolean removeUser(String username, Credentials creds) {
+    /**
+     * Takes a username and their provided credentials, ensures a match, then removes the user from the database.
+     *
+     * @param username The username to be passed in
+     * @param creds The provided credentials
+     * @return True if the operation succeeds.
+     * @throws InvalidRequestException when there is invalid credentials provided
+     * @author Austin Knauer
+     */
+    public boolean removeUser(String username, Credentials creds) throws InvalidRequestException{
 
         if (!username.equals(creds.getUsername())) {
             throw new InvalidRequestException("Submitted username does not match currently logged in user.");
@@ -67,6 +115,12 @@ public class UserService {
         }
     }
 
+    /**
+     *
+     * @param recipeDTO
+     * @param username
+     * @return
+     */
     public UserDTO addFavorite(RecipeDTO recipeDTO, String username) {
         Optional<Recipe> _recipe = recipeRepository.findByLabelAndUrlAndImage(recipeDTO.getLabel(), recipeDTO.getUrl(), recipeDTO.getImage());
         Optional<User> _user = Optional.ofNullable(userRepository.findUserByUsername(username));
